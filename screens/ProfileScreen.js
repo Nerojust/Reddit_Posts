57fbfa7278a2f74eb2545cd62cfa7894a43557c4
nerojust <nerojust4@gmail.com>
2019-08-22 08:52:24 +0100

    fixed bug with loading images in detail component

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View,} from 'react-native';
+import {ActivityIndicator, Image, StyleSheet, Text, View,} from 'react-native';
 
-
-export default class Profile extends React.Component {
+export default class AnotherScreen extends React.Component {
     static navigationOptions = {
         title: 'Details',
     };
-    state: {
-        isLoading: false,
+    state = {
+        loading: true,
+        message: 'I am state',
     };
 
     render() {
         return (
             <View style={styles.container}>
+                {this.state.loading &&
+                <ActivityIndicator size="large" color="#0000ff"/>
+                }
+
                 <Text style={styles.textTitle}>
                     {this.props.navigation.getParam('title', 'no title')}
                 </Text>
                 <Image
                     style={{width: '100%', height: '70%'}}
-                    source={{uri: this.props.navigation.getParam('imageSrc', '')}}
+                    source={{uri: this.props.navigation.getParam('imageSrc', 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjIvLiXm5TkAhXCzIUKHffRDeQQjRx6BAgBEAQ&url=https%3A%2F%2Fpngtree.com%2Ffree-png-vectors%2Floading&psig=AOvVaw2f_RVshoWsQWjdA1lBqSgC&ust=1566485412419552')}}
                 />
+
             </View>
         );
     }
@@ -29,7 +34,7 @@ const styles = StyleSheet.create({
     container: {
         paddingHorizontal: 20,
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#fdf4ff',
         // alignItems: 'center',
         // justifyContent: 'center',
     },
